7e8c5f14cd741760d8d08c82fc42e3743e28d004
Valeriy_Yakovlev <Valera163Samara1@yandex.ru>
2021-05-29 18:31:30 +0400

    Изменил тест

--- src/test/java/Tests/GoogleTests.java
+++ src/test/java/Tests/GoogleTests.java
@@ -1,16 +1,44 @@
 package Tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class GoogleTests {
+
+    private static WebDriver driver;
+    @BeforeAll
+    public static void init(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+
+    }
+
+
+
     @Test
     public void test1(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
-        driver.quit();
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
+        if ( 10 > driver.findElements(By.cssSelector("#search .g")).size() ){
+            System.out.println("Pro");
+        }
 
+
+
+    }
+    @AfterAll
+    public static void teardown(){
+        driver.quit();
     }
 }
