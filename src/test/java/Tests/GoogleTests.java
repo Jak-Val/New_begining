1fa455eedd8991d094a6a460f825ea178ed18988
Valeriy_Yakovlev <Valera163Samara1@yandex.ru>
2021-05-29 17:32:14 +0400

    ''

---
+++ src/test/java/Tests/GoogleTests.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTests {
+    @Test
+    public void test1(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.quit();
+
+    }
+}
